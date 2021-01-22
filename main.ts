93b2c3413039a12fe9631e958d2c4fd0afe1c66f
taurterus <77512286+taurterus@users.noreply.github.com>
2021-01-22 15:15:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let PotentiometerVal = 0
+basic.showString("Hello!")
 basic.forever(function () {
     PotentiometerVal = pins.analogReadPin(AnalogPin.P0)
     pins.analogWritePin(AnalogPin.P2, PotentiometerVal)
